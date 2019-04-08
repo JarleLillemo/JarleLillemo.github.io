9cdf6171d0dfe7718a9a0dc7ad623ad7fb1421d6
JarleLillemo <lillejarle@gmail.com>
2019-04-08 14:45:58 +0200

    Legge til prosjekt
    Teste om link til nettside fungerer

---
+++ WordStory/Main.js
@@ -0,0 +1,27 @@
+
+const inputWord = document.querySelector("#words");
+const spansAdjektiv = document.querySelectorAll("span.adjektiv")
+let counter = 0;
+
+function words() {
+  var word = inputWord.value;
+
+  if(spansAdjektiv.length > counter) {
+    spansAdjektiv[counter].innerText = word;
+    counter += 1;
+  } else {
+    alert("Alle ord brukt opp");
+  }
+
+  inputWord.value = "";
+}
+
+inputWord.addEventListener("keydown", function(e) {
+  if(e.keyCode === 13) {
+    words();
+  }
+});
+
+function nextSite() {
+  window.location='nextSite.html';
+}
